943e0a4c4eb6bbf8a162a94ef8151d195d19609b
Kevin Bretney <kevinbretney@gmail.com>
2018-09-21 19:40:43 -0700

    logic was super broken so I went crazy and brought everything into a class so no more dumb globals, and used a state machine like a real coder

--- marsvideo.py
+++ marsvideo.py
@@ -14,7 +14,6 @@ button_1 = 5
 button_2 = 6
 button_3 = 13
 button_4 = 19
-logic_state = 0
 
 background = ("/home/pi/Documents/mars/MtM-transparent-white.png")
 countdown = ("/home/pi/Documents/mars/countdown.mp4")
@@ -35,145 +34,164 @@ menus = ["Start Game",
          "Start Inf Boost Game",
          "Stop Game"]
 
-# Initialize GPIOs on RPi
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
+class Videoplayer:
+    def __init__(self):
+        self.logic_state = 0
+        self.boost = 0
+        self.infinite = 0
+        self.game_state = 99
+        self.is_menu = 0
 
-GPIO.setup(game_start, GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(boost_active, GPIO.OUT, initial=GPIO.LOW)
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)
 
-GPIO.setup(button_1, GPIO.IN)
-GPIO.setup(button_2, GPIO.IN)
-GPIO.setup(button_3, GPIO.IN)
-GPIO.setup(button_4, GPIO.IN)
+        GPIO.setup(game_start, GPIO.OUT, initial=GPIO.LOW)
+        GPIO.setup(boost_active, GPIO.OUT, initial=GPIO.LOW)
 
-# Create video player instance
-player = OMXPlayer(after, 
-        dbus_name='org.mpris.MediaPlayer2.omxplayer1')
-player.stop()
+        GPIO.setup(button_1, GPIO.IN)
+        GPIO.setup(button_2, GPIO.IN)
+        GPIO.setup(button_3, GPIO.IN)
+        GPIO.setup(button_4, GPIO.IN)
+        
+        GPIO.add_event_detect(button_1, GPIO.FALLING, callback=self.button_callback)
+        GPIO.add_event_detect(button_2, GPIO.FALLING, callback=self.button_callback)
+        GPIO.add_event_detect(button_3, GPIO.FALLING, callback=self.button_callback)
+        GPIO.add_event_detect(button_4, GPIO.FALLING, callback=self.button_callback)
+        
+        self.player = OMXPlayer(after, 
+            dbus_name='org.mpris.MediaPlayer2.omxplayer1')
+        self.player.stop()
+        
+        self.mylcd = I2C_LCD_driver.lcd()
+        self.mylcd.lcd_clear()
 
-# Initialize LCD display
-mylcd = I2C_LCD_driver.lcd()
-mylcd.lcd_clear()
+        self.image = Popen(backgroundload)
 
-# Load the background image
-image = Popen(backgroundload)
-
-def am_i_playing():
-    try:
-        if player.is_playing():
-            return 1
-        else:
+    def am_i_playing(self):
+        try:
+            if self.player.is_playing():
+                return 1
+            else:
+                return 0
+        except:
             return 0
-    except:
-        return 0
 
-def wait_for_video():
-    time.sleep(1)
-    play_state = am_i_playing()
-    while play_state:
-        play_state = am_i_playing()
+    def wait_for_video(self):
+        time.sleep(1)
+        play_state = self.am_i_playing()
+        while play_state:
+            play_state = self.am_i_playing()
 
-def main_menu():
-    global logic_state
-    # print("Main Menu")
-    logic_state = 0
-    mylcd.lcd_clear()
-    time.sleep(.1)
-    for i in range(1, 5):
-        update_display(i-1, i, 0)
+    def main_menu(self):
+        # print("Main Menu")
+        self.mylcd.lcd_clear()
         time.sleep(.1)
-    GPIO.output(game_start, 0)
-    GPIO.output(boost_active, 0)
-
-def start_game(boost,infinite):
-    # print(str(boost)+":"+str(infinite))
-    if infinite:
-        # print("Infinite")
-        GPIO.output(game_start, 1)
-        if boost:
-            GPIO.output(boost_active, 1)
-    else:
-        # print("Single")
-        if boost:
-            # print("Boost")
-            player.load(introboost)
-        else:
-            # print("Normal")
-            player.load(intro)
-        wait_for_video()
-
-        GPIO.output(game_start, 1)
-        if boost:
-            GPIO.output(boost_active, 1)
-        player.load(countdown)
-        wait_for_video()
-
+        for i in range(1, 5):
+            self.update_display(i-1, i, 0)
+            time.sleep(.1)
         GPIO.output(game_start, 0)
         GPIO.output(boost_active, 0)
-        player.load(after)
-        wait_for_video()
+        self.is_menu = 1
 
-        main_menu()
+    def start_game(self, channel):
+        # print(str(channel))
+        self.update_display(4, 1, 1)
+        self.logic_state=1
+        if channel == button_1:
+            self.boost = 0
+            self.infinite = 0
+        elif channel == button_2:
+            self.boost = 1
+            self.infinite = 0
+        elif channel == button_3:
+            self.boost = 0
+            self.infinite = 1
+        elif channel == button_4:
+            self.boost = 1
+            self.infinite = 1
+        self.game_state = 0
+        self.is_menu = 0
 
-def update_display(entry, line, clear):
-    if clear:
-        mylcd.lcd_clear()
-    mylcd.lcd_display_string(menus[entry], line, 0)
+    def stop_game(self):
+        self.logic_state = 0
+        self.game_state = -1
+        if self.infinite == 0:
+            self.player.stop()
+        self.main_menu()
+        
 
-def button_1_callback(channel):
-    global logic_state
-    if logic_state:
-        main_menu()
-    else:
-        update_display(4, 1, 1)
-        logic_state=1
-        start_game(0,0)
+    def get_logic_state(self):
+        return self.logic_state
 
-def button_2_callback(channel):
-    global logic_state
-    if logic_state:
-        main_menu()
-    else:
-        update_display(4, 1, 1)
-        logic_state=1
-        start_game(1,0)
+    def update_display(self,entry, line, clear):
+        if clear:
+            self.mylcd.lcd_clear()
+        self.mylcd.lcd_display_string(menus[entry], line, 0)
 
-def button_3_callback(channel):
-    global logic_state
-    if logic_state:
-        main_menu()
-    else:
-        update_display(4, 1, 1)
-        logic_state=1
-        start_game(0,1)
+    def button_callback(self, channel):
+        if self.get_logic_state():
+            self.stop_game()
+        else:
+            self.start_game(channel)
 
-def button_4_callback(channel):
-    global logic_state
-    if logic_state:
-        main_menu()
-    else:
-        update_display(4, 1, 1)
-        logic_state=1
-        start_game(1,1)
-
-GPIO.add_event_detect(button_1, GPIO.FALLING, callback=button_1_callback)
-GPIO.add_event_detect(button_2, GPIO.FALLING, callback=button_2_callback)
-GPIO.add_event_detect(button_3, GPIO.FALLING, callback=button_3_callback)
-GPIO.add_event_detect(button_4, GPIO.FALLING, callback=button_4_callback)
+    def state_machine(self):
+        # print(videoplayer.game_state)
+        if self.game_state == 0:
+            if self.infinite:
+                # print("Infinite")
+                GPIO.output(game_start, 1)
+                if self.boost:
+                    GPIO.output(boost_active, 1)
+                self.game_state = -1
+            else:
+                self.game_state = 1
+                # print("Single")
+                if self.boost:
+                    # print("Boost")
+                    self.player.load(introboost)
+                else:
+                    # print("Normal")
+                    self.player.load(intro)
+                time.sleep(1)
+                
+        elif self.game_state == 1:
+            if self.am_i_playing() == 0:
+                self.game_state = 2
+                GPIO.output(game_start, 1)
+                if self.boost:
+                    GPIO.output(boost_active, 1)
+                self.player.load(countdown)
+                time.sleep(1)
+                
+        elif self.game_state == 2:
+            if self.am_i_playing() == 0:
+                self.game_state = 3
+                GPIO.output(game_start, 0)
+                GPIO.output(boost_active, 0)
+                self.player.load(after)
+                time.sleep(1)
+                
+        elif self.game_state == 3:
+            if self.is_menu == 0:
+                self.main_menu()
+                self.game_state = -1
 
 def main():
-    buttons=0
-    main_menu()
-    while buttons!=8:
-        time.sleep(1)
+    videoplayer = Videoplayer()
+    buttons = 0
+    videoplayer.main_menu()
+    while buttons != 20:
+        time.sleep(.2)
         if GPIO.input(button_4):
-            buttons=0
+            buttons = 0
         else:
-            buttons+=1
+            buttons += 1
+        videoplayer.state_machine()    
     GPIO.cleanup()
-    mylcd.lcd_clear()
-    image.kill()
+    videoplayer.mylcd.lcd_clear()
+    videoplayer.image.kill()
+    if videoplayer.am_i_playing():
+        videoplayer.player.stop()
 
 
 if __name__ == "__main__":
